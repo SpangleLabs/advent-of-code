e372bf2fce30704b917ec43cfd4fe2ac7c85859f
Joshua Coales <joshua@coales.co.uk>
2021-12-02 07:21:31 +0000

    2021, Day 2 part 2

---
+++ 2021/02-2.py
@@ -0,0 +1,39 @@
+import dataclasses
+
+from utils import load_lines
+
+
+@dataclasses.dataclass
+class Coords:
+    horizontal: int
+    depth: int
+    aim: int
+
+    def forward(self, num) -> "Coords":
+        self.horizontal += num
+        self.depth += num * self.aim
+        return self
+
+    def up(self, num) -> "Coords":
+        self.aim -= num
+        return self
+
+    def down(self, num) -> "Coords":
+        self.aim += num
+        return self
+
+    def route_command(self, cmd, num) -> None:
+        {
+            "forward": self.forward,
+            "up": self.up,
+            "down": self.down
+        }[cmd](num)
+
+
+if __name__ == "__main__":
+    coords = Coords(0, 0, 0)
+    for line in load_lines():
+        cmd, num_str = line.split()
+        num = int(num_str)
+        coords.route_command(cmd, num)
+    print(coords.horizontal * coords.depth)
