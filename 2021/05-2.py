c088226c1098af4faa71f9f0fdc021355127e712
Joshua Coales <joshua@coales.co.uk>
2021-12-05 11:57:42 +0000

    Pulling some common Line2D and Map2D stuff up to utils_coords

--- 2021/05-2.py
+++ 2021/05-2.py
@@ -1,16 +1,13 @@
 import dataclasses
-from functools import cached_property
 from typing import List
 
 from utils import load_lines
-from utils_coords import Coords2D
+from utils_coords import Coords2D, Line2D, Map2D
 
 
-class Map:
+class Map(Map2D):
     def __init__(self, width: int, height: int) -> None:
-        self.map = []
-        for _ in range(height):
-            self.map.append([0 for _ in range(width)])
+        super().__init__(width, height, 0)
 
     def render(self) -> str:
         return "\n".join(
@@ -46,62 +43,10 @@ class Map:
 
 
 @dataclasses.dataclass
-class Line:
-    start: Coords2D
-    end: Coords2D
-
-    @cached_property
-    def min_x(self) -> int:
-        return min(self.start.x, self.end.x)
-
-    @cached_property
-    def min_y(self) -> int:
-        return min(self.start.y, self.end.y)
-
-    @cached_property
-    def max_x(self) -> int:
-        return max(self.start.x, self.end.x)
-
-    @cached_property
-    def max_y(self) -> int:
-        return max(self.start.y, self.end.y)
-
-    @cached_property
-    def len_x(self) -> int:
-        return self.max_x - self.min_x
-
-    @cached_property
-    def len_y(self) -> int:
-        return self.max_y - self.min_y
-
-    @cached_property
-    def vertical(self) -> bool:
-        return self.start.x == self.end.x
-
-    @cached_property
-    def horizontal(self) -> bool:
-        return self.start.y == self.end.y
-
-    @cached_property
-    def diagonal(self) -> bool:
-        return self.len_x == self.len_y
-
-    @cached_property
-    def leading_diagonal(self) -> bool:
-        if not self.diagonal:
-            raise ValueError
-        return self.start in [Coords2D(self.min_x, self.min_y), Coords2D(self.max_x, self.max_y)]
+class Line(Line2D):
 
     def is_valid(self) -> bool:
         return self.horizontal or self.vertical or self.diagonal
-
-    @classmethod
-    def from_input_line(cls, input_line: str) -> "Line":
-        start, end = input_line.split(" -> ", 1)
-        return cls(
-            Coords2D.from_input_line(start),
-            Coords2D.from_input_line(end)
-        )
 
 
 def find_map_dimensions(lines: List[Line]) -> Coords2D:
--- 2021/utils_coords.py
+++ 2021/utils_coords.py
@@ -1,4 +1,6 @@
 import dataclasses
+from functools import cached_property
+from typing import Union, Optional
 
 
 @dataclasses.dataclass
@@ -16,3 +18,66 @@ class Coords2D:
 
     def __eq__(self, other: "Coords2D") -> bool:
         return isinstance(other, Coords2D) and self.x == other.x and self.y == other.y
+
+
+@dataclasses.dataclass
+class Line2D:
+    start: Coords2D
+    end: Coords2D
+
+    @cached_property
+    def min_x(self) -> int:
+        return min(self.start.x, self.end.x)
+
+    @cached_property
+    def min_y(self) -> int:
+        return min(self.start.y, self.end.y)
+
+    @cached_property
+    def max_x(self) -> int:
+        return max(self.start.x, self.end.x)
+
+    @cached_property
+    def max_y(self) -> int:
+        return max(self.start.y, self.end.y)
+
+    @cached_property
+    def len_x(self) -> int:
+        return self.max_x - self.min_x
+
+    @cached_property
+    def len_y(self) -> int:
+        return self.max_y - self.min_y
+
+    @cached_property
+    def vertical(self) -> bool:
+        return self.start.x == self.end.x
+
+    @cached_property
+    def horizontal(self) -> bool:
+        return self.start.y == self.end.y
+
+    @cached_property
+    def diagonal(self) -> bool:
+        return self.len_x == self.len_y
+
+    @cached_property
+    def leading_diagonal(self) -> bool:
+        if not self.diagonal:
+            raise ValueError
+        return self.start in [Coords2D(self.min_x, self.min_y), Coords2D(self.max_x, self.max_y)]
+
+    @classmethod
+    def from_input_line(cls, input_line: str) -> "Line2D":
+        start, end = input_line.split(" -> ", 1)
+        return cls(
+            Coords2D.from_input_line(start),
+            Coords2D.from_input_line(end)
+        )
+
+
+class Map2D:
+    def __init__(self, width: int, height: int, fill: Optional[Union[str, int]] = None) -> None:
+        self.map = []
+        for _ in range(height):
+            self.map.append([fill for _ in range(width)])
