25d67ec1fc8d6a896fe4e95cd6fe22eef7f8850f
Joshua Coales <joshua@coales.co.uk>
2021-12-15 18:48:39 +0000

    Only print this year's leaderboards

--- scripts/view_leaderboard_times.py
+++ scripts/view_leaderboard_times.py
@@ -1,16 +1,15 @@
 import json
 
-from scripts.leaderboard import Leaderboard, CONFIG_FILE
+from leaderboard import Leaderboard, CONFIG_FILE
 
 if __name__ == "__main__":
     input_year = 2021
-    for input_year in range(2015, 2022):
-        with open(CONFIG_FILE, "r") as conf_file:
-            conf = json.load(conf_file)
-        for lid in conf["leaderboards"]:
-            leaderboard = Leaderboard.load_from_api_or_cache(str(lid), conf["session_cookie"], input_year)
-            print(leaderboard.render(
-                show_times=True,
-                show_durations=True,
-                show_diff=True
-            ))
+    with open(CONFIG_FILE, "r") as conf_file:
+        conf = json.load(conf_file)
+    for lid in conf["leaderboards"]:
+        leaderboard = Leaderboard.load_from_api_or_cache(str(lid), conf["session_cookie"], input_year)
+        print(leaderboard.render(
+            show_times=True,
+            show_durations=True,
+            show_diff=True
+        ))
