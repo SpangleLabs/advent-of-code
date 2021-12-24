8011a220e25e830fc51a28138076eeea9c6ade83
Joshua Coales <joshua@coales.co.uk>
2021-12-24 11:22:05 +0000

    Adding wrap method, which was used by 2021, day 21

--- utils/math.py
+++ utils/math.py
@@ -1,2 +1,12 @@
 def triangle_number(inp: int) -> int:
     return int((inp ** 2 / 2) + (inp / 2))
+
+
+def wrap(num: int, mod: int) -> int:
+    """
+    This is like modulo, but plus one.
+    :param num: Number to wrap
+    :param mod: Maximum output to allow
+    :return: The input number, wrapped by the mod number
+    """
+    return 1 + ((num - 1) % mod)
